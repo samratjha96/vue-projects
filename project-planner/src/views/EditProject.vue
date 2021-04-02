b063f1ee95f2414753d470092fd39c08062100e0
Samrat Jha <samrat.jha@appian.com>
2021-04-02 19:44:48 -0400

    feat: Add UpdateProject capability and link to button

---
+++ project-planner/src/views/EditProject.vue
@@ -0,0 +1,80 @@
+<template>
+    <form>
+    <label>Title: </label>
+    <input type="text" v-model="title" required />
+    <label>Details:</label>
+    <textarea v-model="details" required></textarea>
+    <button>Update Project</button>
+  </form>
+</template>
+
+<script>
+    export default {
+        props: {
+            id: {
+                type: Number
+            },
+        },
+        data() {
+            return {
+                uri: 'http://localhost:3000/projects/' + this.id,
+                title: '',
+                details: '',
+            }
+        },
+        mounted() {
+            fetch(this.uri)
+            .then(res => res.json())
+            .then(data => {
+                this.title = data.title
+                this.details = data.details
+            }) 
+        }
+    }
+</script>
+
+<style>
+form {
+  background: white;
+  padding: 20px;
+  border-radius: 10px;
+}
+
+label {
+  display: block;
+  color: #bbb;
+  text-transform: uppercase;
+  font-size: 14px;
+  font-weight: bold;
+  letter-spacing: 1px;
+  margin: 20px 0 10px 0;
+}
+
+input {
+  padding: 10px;
+  border: 0;
+  border-bottom: 1px solid #ddd;
+  width: 100%;
+  box-sizing: border-box;
+}
+
+textarea {
+  border: 1px solid #ddd;
+  padding: 10px;
+  width: 100%;
+  box-sizing: border-box;
+  height: 100px;
+}
+
+form button {
+	display: block;
+	margin: 20px auto 0;
+	background: #00ce89;
+	color: white;
+	padding: 10px;
+	border: 0;
+	border-radius: 6px;
+	font-size: 16px;
+	font-weight: bold;
+}
+</style>
