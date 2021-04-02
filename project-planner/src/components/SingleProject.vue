b89c8914d9a6d138ac9ab42d6a735459027bd6a3
Samrat Jha <samrat.jha@appian.com>
2021-04-02 17:40:51 -0400

    feat: add icons and styling for icons

--- project-planner/src/components/SingleProject.vue
+++ project-planner/src/components/SingleProject.vue
@@ -2,6 +2,11 @@
   <div class="project">
     <div class="actions">
       <h3 @click="showDetails = !visible">{{ project.title }}</h3>
+			<div class="icons">
+				<span class="material-icons">edit</span>
+				<span class="material-icons">delete</span>
+				<span class="material-icons">done</span>
+			</div>
     </div>
 		<div v-if="showDetails" class="details">
 			<p>{{ project.details }}</p>
@@ -36,6 +41,24 @@ export default {
 		box-shadow: 1px 2px 3px rgba(0, 0, 0, 0.05);
 		border-left: 4px solid #e90074
 	}
+
+	.actions {
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+	}
+
+	.material-icons {
+		font-size: 24px;
+		margin-left: 10px;
+		color: #bbb;
+		cursor: pointer;
+	}
+
+	.material-icons:hover {
+		color: #777;
+	}
+
 	h3 {
 		cursor: pointer;
 	}
