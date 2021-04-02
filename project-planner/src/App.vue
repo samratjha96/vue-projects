792d76f4babfc2ec175df4fc2f63c6aa0c4fd7de
Samrat Jha <samrat.jha@appian.com>
2021-04-02 16:41:22 -0400

    config: initializing project-planner

---
+++ project-planner/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <router-view />
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  components: {
+  }
+}
+</script>
+
+<style>
+body {
+  background: #f2f2f2
+}
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  max-width: 600px;
+  margin: 0 auto;
+  color: #555;
+  margin-top: 60px;
+}
+</style>
---
+++ project-planner/src/views/Home.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="home">
+    <div v-if="projects.length">
+      <div v-for="project in projects" :key="project.id">
+        <p>{{ project.title }}</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  props: {
+    msg: String
+  },
+  data() {
+    return {
+      projects: []
+    }
+  },
+  mounted() {
+    fetch("http://localhost:3000/projects")
+    .then((res) => res.json())
+    .then(data => this.projects = data)
+    .catch(err => console.log(err.message))
+  }
+}
+</script>
