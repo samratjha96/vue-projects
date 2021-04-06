14680f6912430fa4cd09ff4da848c499a1f81435
Samrat Jha <samrat.jha@appian.com>
2021-04-05 20:32:36 -0400

    feat: Add navbar and logout skeleton

---
+++ live-chat-app/src/components/Navbar.vue
@@ -0,0 +1,33 @@
+<template>
+  <nav>
+    <div>
+      <p>Hey there {{ displayName }}...</p>
+      <div class="email">Current logged in as {{ email }}</div>
+    </div>
+    <button @click="handleClick">Logout</button>
+    <div class="error">{{ error }}</div>
+  </nav>
+</template>
+
+<script>
+import useLogout from "@/lib/useLogout";
+
+export default {
+  setup() {
+    const { logout, error } = useLogout();
+
+    const handleClick = async () => {
+      await logout();
+      if (!error.value) {
+        console.log("User logged out");
+      }
+    };
+    return {
+      handleClick,
+      error,
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
