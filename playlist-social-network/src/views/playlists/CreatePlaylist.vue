ba58439aeb88250823694423425e02520d1880fe
Samrat Jha <samrat.jha@appian.com>
2021-04-11 20:17:56 -0400

    feat: CreateNewPlaylist form with file upload

---
+++ playlist-social-network/src/views/playlists/CreatePlaylist.vue
@@ -0,0 +1,67 @@
+<template>
+  <form @submit.prevent="handleSubmit">
+      <h4>Create New Playlist</h4>
+      <input type="text" required placeholder="Playlist title" v-model="title">
+      <textarea required placeholder="Playlist description..." v-model="description"></textarea>
+      <label>Upload playlist cover image</label>
+      <input type="file" @change="handleChange">
+      <div class="error">{{ fileError }}</div>
+      <div class="error"></div>
+      <button>Create</button>
+  </form>
+</template>
+
+<script>
+import { ref } from '@vue/reactivity'
+export default {
+    setup() {
+        const title = ref("");
+        const description = ref("");
+        const file = ref(null);
+        const fileError = ref(null);
+
+        const handleSubmit = () => {
+            if(file.value) {
+                console.log("Submitted", title.value, description.value, file.value)
+            }
+        }
+
+        const handleChange = (event) => {
+            const selected = event.target.files[0];
+            const allowedFileTypes = ['image/png', 'image/jpg', 'image/jpeg']
+            if(selected && allowedFileTypes.includes(selected.type)) {
+                file.value = selected;
+                fileError.value = null;
+            } else {
+                file.value = null;
+                fileError.value = "Please select an image file (png or jpeg)";
+            }
+        }
+
+        return {
+            title,
+            description,
+            handleSubmit,
+            handleChange,
+            fileError
+        }
+    }
+}
+</script>
+
+<style>
+    input[type="file"] {
+        border: 0;
+        padding: 0;
+    }
+
+    label {
+        font-size: 12px;
+        display: block;
+        margin-top: 30px;
+    }
+
+    button {
+        margin-top: 20px;
+    }
+</style>
