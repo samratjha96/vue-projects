353c3d607d40c0b45e4b12d1907d56f4310e5325
Samrat Jha <samrat.jha@appian.com>
2021-04-11 22:23:19 -0400

    chore: Add route guard to routes and Create Playlist button

--- playlist-social-network/src/router/index.js
+++ playlist-social-network/src/router/index.js
@@ -4,11 +4,25 @@ import Login from "@/views/auth/Login.vue";
 import Signup from "@/views/auth/Signup.vue";
 import CreatePlaylist from "@/views/playlists/CreatePlaylist.vue";
 
+
+// Route guards
+import { projectAuth } from "@/firebase/config"
+
+const requireAuth = (to, from, next) => {
+  const user = projectAuth.currentUser
+  if(!user) {
+    next({name: 'Login'});
+  } else {
+    next();
+  }
+}
+
 const routes = [
   {
     path: "/",
     name: "Home",
-    component: Home
+    component: Home,
+    beforeEnter: requireAuth
   },
   {
     path: "/login",
@@ -23,7 +37,8 @@ const routes = [
   {
     path: "/playlists/create",
     name: "CreatePlaylist",
-    component: CreatePlaylist
+    component: CreatePlaylist,
+    beforeEnter: requireAuth
   }
 ];
 
